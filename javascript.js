fb4d07a488f18475d8e6d58891a0bb0e0d2aa827
ind3ci5ive <39619831+ind3ci5ive@users.noreply.github.com>
2025-03-18 16:35:35 -0500

    Add correct sizing for grid container and add grid button

--- javascript.js
+++ javascript.js
@@ -20,6 +20,16 @@ for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.style.width = "16px";
     div.style.height = "16px";
+    div.style.margin ="0px";
     container.appendChild(div);
 
+    // add styles to object initialization instead of the CSS sheet
+
 }
+
+let body = document.querySelector("body");
+
+let sizeButton = document.createElement("button");
+sizeButton.innerHTML = "Change Grid Size";
+body.appendChild(sizeButton);
+
